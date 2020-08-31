6a0dfb536d024d0ab88dc6088aec3bf11614a573
Zack Jackson <zackary.l.jackson@gmail.com>
2020-08-31 16:58:03 -0700

    use require context to crawl directories and vanilla require to require them as externals

--- host/src/index.js
+++ host/src/index.js
@@ -1,1 +1,24 @@
+const path = require('path')
 console.log("Hello");
+
+function importAll(contextLoader) {
+  const allComponent = contextLoader.keys().reduce((acc, id) => {
+    const cleanID = id.substr(1);
+    acc.push(__non_webpack_require__(path.join('../plugins', cleanID)))
+
+    return acc;
+  }, []);
+  return allComponent;
+}
+
+// could also use main.js, since it exposes the same thing as remotes do.
+const contextLoader = require.context(
+  "../plugins",
+  true,
+  /remoteEntry.js$/
+);
+
+const [plugin1] = importAll(contextLoader)
+plugin1.get('./plugin').then(factory => factory()).then(({sample}) => {
+  sample()
+})
