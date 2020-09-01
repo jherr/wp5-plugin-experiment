2d3df54bea0408714e95c447621783477ff93188
Zack Jackson <zackary.l.jackson@gmail.com>
2020-08-31 17:27:45 -0700

    use require context to crawl directories and vanilla require to require them as externals

--- host/src/index.js
+++ host/src/index.js
@@ -1,14 +1,27 @@
 const path = require('path')
 console.log("Hello");
 
-function importAll(contextLoader) {
-  const allComponent = contextLoader.keys().reduce((acc, id) => {
+const importAll = async (contextLoader) => {
+  if (__webpack_share_scopes__.default) {
+    await __webpack_init_sharing__("default");
+  }
+  const allPlugins = contextLoader.keys().map((id) => {
     const cleanID = id.substr(1);
-    acc.push(__non_webpack_require__(path.join('../plugins', cleanID)))
-
-    return acc;
-  }, []);
-  return allComponent;
+    return new Promise((resolve) => {
+      const container = __non_webpack_require__(path.join('../plugins', cleanID))
+      const initContainer = new Promise((resolve) => {
+        if (__webpack_share_scopes__.default) {
+          resolve(container.init(__webpack_share_scopes__.default))
+        } else {
+          resolve()
+        }
+      })
+      initContainer.then(() => {
+        resolve(container)
+      })
+    })
+  })
+  return Promise.all(allPlugins)
 }
 
 // could also use main.js, since it exposes the same thing as remotes do.
@@ -17,8 +30,13 @@ const contextLoader = require.context(
   true,
   /remoteEntry.js$/
 );
+//could try top-level awaits
+const start = async () => {
+  const [plugin1] = await importAll(contextLoader)
+  plugin1.get('./plugin').then(factory => factory()).then(({sample}) => {
+    sample()
+  })
+}
 
-const [plugin1] = importAll(contextLoader)
-plugin1.get('./plugin').then(factory => factory()).then(({sample}) => {
-  sample()
-})
+start()
+
